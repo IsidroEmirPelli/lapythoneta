1af7c25424ca8883fe648e94829cf7c11634f3a3
Isidro Emir Pelli <isidroemirpelli@gmail.com>
2022-06-16 16:19:44 -0300

    Dolar

---
+++ assets/js/inicio.js
@@ -0,0 +1,6 @@
+function changeStyle(){
+    const element = document.getElementById('changeStyle');
+    console.log(element)
+    const display = element.style.display
+    console.log(display)
+}
